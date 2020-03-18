0ac88f3d50a418ff545d27d0a75690298d783239
SH Kim <kshgroup@kshgroup.kr>
2020-03-18 18:25:45 +0900

    Tic-Tac-Toe

---
+++ 07_00_틱택토/TdComponent.vue
@@ -0,0 +1,60 @@
+<template>
+    <td @click="onClickTd">{{ cellData }}</td>
+</template>
+
+<script>
+    export default {
+        props: {
+            rowIndex: Number,
+            cellData: String,
+            cellIndex: Number
+        },
+        methods: {
+            onClickTd() {
+                if (this.cellData) return;
+
+                const rootData = this.$root.$data;
+                this.$set(rootData.tableData[this.rowIndex], this.cellIndex, rootData.turn);
+
+                let win = false;
+
+                if (rootData.tableData[this.rowIndex][0] === rootData.turn && rootData.tableData[this.rowIndex][1] === rootData.turn && rootData.tableData[this.rowIndex][2] === rootData.turn) {
+                    win = true;
+                }
+                if (rootData.tableData[0][this.cellIndex] === rootData.turn && rootData.tableData[1][this.cellIndex] === rootData.turn && rootData.tableData[2][this.cellIndex] === rootData.turn) {
+                    win = true;
+                }
+                if (rootData.tableData[0][0] === rootData.turn && rootData.tableData[1][1] === rootData.turn && rootData.tableData[2][2] === rootData.turn) {
+                    win = true;
+                }
+                if (rootData.tableData[0][2] === rootData.turn && rootData.tableData[1][1] === rootData.turn && rootData.tableData[2][0] === rootData.turn) {
+                    win = true;
+                }
+
+                let winner = "";
+                if (win) {
+                    winner = rootData.turn;
+                    this.$root.clearTurn();
+                } else {
+                    let all = true;
+
+                    rootData.tableData.forEach((row) => {
+                        row.forEach((cell) => {
+                            if (!cell) {
+                                all = false;
+                            }
+                        });
+                    });
+
+                    if (all) {
+                        this.$root.clearTurn();
+                    } else {
+                        rootData.turn = rootData.turn === "O" ? "X" : "O";
+                    }
+                }
+
+                rootData.winner = winner;
+            }
+        }
+    }
+</script>
